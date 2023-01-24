73b8fc3afd108fe6862c754fcec0bc5aa8176541
rakanabusaidah <rakanmya@gmail.com>
2023-01-24 22:15:56 +0300

    first commit

---
+++ AdvanceAPI.API/Models/QueryParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace AdvanceAPI.API.Models
+{
+    public class QueryParameters
+    {
+        const int _maxSize = 100;
+        private int _size = 50;
+
+        public int Page { get; set; } = 1;
+        // should set value of size that is not more than _maxSize
+        public int Size
+        {
+            get { return _size; }
+            set
+            {
+                _size = Math.Min(_maxSize, value);
+            }
+        }
+    }
+}
